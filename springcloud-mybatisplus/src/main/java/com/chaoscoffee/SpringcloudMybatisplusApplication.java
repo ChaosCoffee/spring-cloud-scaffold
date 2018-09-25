3105ab7a213f64138d89c1503867817f966fd69e
chaoshen <chaoshen@mo9.com>
2018-09-25 10:23:21 +0800

    add project springcloud-mybatisplus

---
+++ springcloud-mybatisplus/src/main/java/com/chaoscoffee/SpringcloudMybatisplusApplication.java
@@ -0,0 +1,12 @@
+package com.chaoscoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudMybatisplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudMybatisplusApplication.class, args);
+    }
+}
